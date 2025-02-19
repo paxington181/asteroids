84b07c94ec1118de4aaef8862240d98bac3674c8
paxington181 <tsandrus@rogers.com>
2025-02-19 18:16:13 -0500

    Lesson 5 complete, code refactored chasing a bug that was a silly mistake

--- main.py
+++ main.py
@@ -1,42 +1,28 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
-from constants import(
-    SCREEN_WIDTH,
-    SCREEN_HEIGHT,
-    ASTEROID_MIN_RADIUS,
-    ASTEROID_KINDS,
-    ASTEROID_SPAWN_RATE,
-    ASTEROID_MAX_RADIUS
-    )
+from constants import *
+from player import *
+
+
 
 def main():
     
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    screen = pygame.display.set_mode(
-        (SCREEN_WIDTH, SCREEN_HEIGHT)
-        )
-
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-
+    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT /2)
 
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(
-            screen,
-            (0, 0, 0) ,
-            rect=None,
-            special_flags=0
-            )
+            
+        screen.fill("black")        
+        player.draw(screen)
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
