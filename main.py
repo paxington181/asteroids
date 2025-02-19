73b721a8cd0697f4a934eac65274c637c28b60e0
paxington181 <tsandrus@rogers.com>
2025-02-18 19:29:28 -0500

    Second Lesson Complete

--- main.py
+++ main.py
@@ -16,6 +16,21 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode(
+        (SCREEN_WIDTH, SCREEN_HEIGHT)
+        )
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(
+            screen,
+            (0, 0, 0) ,
+            rect=None,
+            special_flags=0
+            )
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
