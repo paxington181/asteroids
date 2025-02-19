3c1a847e43bc8c407588e744c92e599dc0c65860
paxington181 <tsandrus@rogers.com>
2025-02-19 14:19:55 -0500

    Lesson 3 FPS Limit

--- main.py
+++ main.py
@@ -20,6 +20,12 @@ def main():
     screen = pygame.display.set_mode(
         (SCREEN_WIDTH, SCREEN_HEIGHT)
         )
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -31,6 +37,7 @@ def main():
             special_flags=0
             )
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
