b4f2bf6d7ce4452d9e541245f07757dd64166ee0
paxington181 <tsandrus@rogers.com>
2025-03-04 11:18:33 -0500

    Ch3 L1 Complete

--- asteroid.py
+++ asteroid.py
@@ -3,10 +3,10 @@ from circleshape import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        super().__init__(self, x, y, radius)
+        super().__init__(x, y, radius)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, white, self.circleshape, 2)
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
-    def move(self, dt):
-        
+    def update(self, dt):
+        self.position += self.velocity * dt
