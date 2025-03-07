e8829e1226a9f9f34b486d564f25b279fad2c579
paxington181 <tsandrus@rogers.com>
2025-03-07 10:54:24 -0500

    need to add shoot method to player

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import pygame
 from constants import *
-from player import Player
+from player import Player, Shot
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 
@@ -17,12 +17,15 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shot = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
 
     Player.containers = (updatable, drawable)
+    Shot.containers = (updatable, drawable)
+
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
